be6f99555df16a4db51a380839b1087a206778c7
zd6515843 <32858918+zd6515843@users.noreply.github.com>
2017-12-06 16:03:27 +0800

    Create lession_str.py

---
+++ PythonSelfStudy/lession_str.py
@@ -0,0 +1,10 @@
+testStr = ' this is a test string ';
+print(testStr+'123');
+print('\t'+testStr);
+
+print(testStr.rstrip()+'123');
+print(testStr.lstrip()+'123');
+print(testStr.strip()+'123');
+
+print(testStr.upper()) #  THIS IS A TEST STRING
+print(testStr.lower()) #  this is a test string
